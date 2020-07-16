4b276e5afbf09ee3a4c7e54c2efaa5e6b7622ded
marcinWS <marcinws@gmail.com>
2020-07-16 07:21:41 -0400

    ADD @script.js get 'href attribute from clicked link (clickedElement)

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
    console.log('links:', links);
 });*/
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -14,8 +15,8 @@ const titleClickHandler = function(event){
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
-    /* [IN PROGRESS] add class 'active' to the clicked link */
-   console.log('clickedElement', clickedElement);
+    /* [DONE] add class 'active' to the clicked link */
+   console.log('clickedElement: ', clickedElement);
    clickedElement.classList.add('active');
    
     /* [DONE] remove class 'active' from all articles */
@@ -26,6 +27,8 @@ const titleClickHandler = function(event){
     }
 
     /* get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log('articleSelector: ', articleSelector);
   
     /* find the correct article using the selector (value of 'href' attribute) */
   
