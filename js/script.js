ffa6c440a22dac8e6390776a2f7f5a98e13226e6
marcinWS <marcinws@gmail.com>
2020-07-16 08:09:30 -0400

    ADD @script.js class'active' to targetArticle

--- js/script.js
+++ js/script.js
@@ -30,11 +30,13 @@ const titleClickHandler = function(event){
     const articleSelector = clickedElement.getAttribute('href');
     console.log('articleSelector: ', articleSelector);
   
-    /* [IN PROGRESS] find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log('targetArticle: ', targetArticle);
 
     /* add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+    
   }
   
   const links = document.querySelectorAll('.titles a');
